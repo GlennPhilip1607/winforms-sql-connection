8f1fe16d743f09e8bc22e67f372fd66a895fa2bd
GlennPhilip1607 <glennphilip08@gmail.com>
2026-03-29 21:14:19 +0800

    Add CRUD operations for student records in Form1
    Implemented add, update, delete, and clear functionality for student records in Form1.

--- TestWins/Form1.cs
+++ TestWins/Form1.cs
@@ -1,13 +1,11 @@
-using System.Drawing.Text;
 using TestWins.Controller;
 
 namespace TestWins;
 
 public partial class Form1 : Form
 {
-    //business
-
     private readonly StudentController controller = new StudentController();
+
     public Form1()
     {
         InitializeComponent();
@@ -21,23 +19,46 @@ public partial class Form1 : Form
 
     private void btnAdd_Click(object sender, EventArgs e)
     {
-
+        controller.add(txtName.Text, txtCourse.Text);
+        loadData();
+        clearFields();
     }
 
     private void btnUpdate_Click(object sender, EventArgs e)
     {
-
+        int id = int.Parse(txtId.Text);
+        controller.update(id, txtName.Text, txtCourse.Text);
+        loadData();
+        clearFields();
     }
+
 
     private void btnDelete_Click(object sender, EventArgs e)
     {
-
-
-
+        int id = int.Parse(txtId.Text);
+        controller.delete(id);
+        loadData();
+        clearFields();
     }
 
-    private void dataGridView1_CellClick(object sender, EventArgs e)
+   
+    private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
     {
+        if (e.RowIndex >= 0)
+        {
+            DataGridViewRow row = dataGridView1.Rows[e.RowIndex];
 
+            txtId.Text = row.Cells["id"].Value.ToString();
+            txtName.Text = row.Cells["name"].Value.ToString();
+            txtCourse.Text = row.Cells["course"].Value.ToString();
+        }
+    }
+
+  
+    private void clearFields()
+    {
+        txtId.Clear();
+        txtName.Clear();
+        txtCourse.Clear();
     }
 }
