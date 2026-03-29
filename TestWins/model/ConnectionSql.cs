299242aa19f2630c640f613afcadfd6b1252e4be
GlennPhilip1607 <glennphilip08@gmail.com>
2026-03-29 21:11:31 +0800

    Refactor ConnectionSql class and method names

--- TestWins/model/ConnectionSql.cs
+++ TestWins/model/ConnectionSql.cs
@@ -1,22 +1,28 @@
-namespace TestWins.Model;
-
-//dotnet add package MySql.Data
 using MySql.Data.MySqlClient;
 
-public class ConnectionSql
+namespace TestWins.Model
 {
-    private readonly string _connectionString = "server=localhost;database=student;uid=root;pwd=root";
+    public class ConnectionSql
+    {
+        private readonly string _connectionString = "server=localhost;database=student;uid=root;pwd=root";
         private MySqlConnection _conn;
 
-    public MySqlConnection connectSql()
-    {
+        public MySqlConnection ConnectSql()
+        {
+            try
+            {
+                Console.WriteLine("Connecting to DB...");
 
-        Console.WriteLine("Connecting to DB");
+                _conn = new MySqlConnection(_connectionString);
+                _conn.Open(); // 
+                Console.WriteLine("Database connection successful");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Database connection failed: " + ex.Message);
+            }
 
-        _conn = new MySqlConnection(_connectionString);
-
-        Console.WriteLine(_conn == null ? "Datbase Connection Failed" : "Database connection successful");
-
-        return _conn;
+            return _conn;
+        }
     }
 }
